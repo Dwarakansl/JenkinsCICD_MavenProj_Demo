83901cac958d9f4f200ea91c05a927ef94f43ee3
dpoor <dpoor@192.168.2.13>
2023-01-27 18:10:16 -0500

    2nd commit by Dwaraka.

--- src/test/java/HelloJenkinsCICD.java
+++ src/test/java/HelloJenkinsCICD.java
@@ -2,7 +2,7 @@
 public class HelloJenkinsCICD {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Jenkins CICD.");
+		System.out.println("Welcome to Jenkins ....");
 
 	}
 
